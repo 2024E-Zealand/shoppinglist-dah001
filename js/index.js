8890bab84aa49261576a10036828cdea241c3334
Daniyal Hussain <dah001@edu.zealand.dk>
2024-10-01 14:20:18 +0200

    opg 1-2

--- js/index.js
+++ js/index.js
@@ -1,2 +1,22 @@
 //Write your Javascript code here
 console.log("Shoppinglist")
+
+const list = document.getElementById("list");
+
+const NewLi = document.createElement("li");
+
+NewLi.id=4;
+NewLi.className = "unhealthy";
+NewLi.textContent = "potatoes";
+
+list.append(NewLi);
+
+
+
+
+
+
+
+
+
+
